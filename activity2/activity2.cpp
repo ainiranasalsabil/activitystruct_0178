946ba6cf4da3e293b25ff15ab8f4e42de78417f7
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-04-02 11:07:08 +0700

    menyimpan data pada variabel mhs.nim dan mhs.nama

--- activity2/activity2.cpp
+++ activity2/activity2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct DetailAlamat {
@@ -11,4 +12,13 @@ struct mahasiswa {
 	string nama;
 	DetailAlamat alamat;
 	int umur;
+};
+
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor mahasiswa : ";
+	getline(cin, mhs.nim);
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+
 };
